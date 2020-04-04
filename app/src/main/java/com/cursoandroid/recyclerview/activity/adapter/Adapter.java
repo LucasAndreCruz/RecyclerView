35d3f1ceb208fc0fcbb53d96e48e4f9fa93a2a5f
LUCAS ANDRÉ <lucas_andre_spfc@hotmail.com>
2020-04-03 23:19:05 -0300

    Commit RecyclerView - Part 3: transformando adapter_lista.xml em uma view

--- app/src/main/java/com/cursoandroid/recyclerview/activity/adapter/Adapter.java
+++ app/src/main/java/com/cursoandroid/recyclerview/activity/adapter/Adapter.java
@@ -1,11 +1,14 @@
 package com.cursoandroid.recyclerview.activity.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.cursoandroid.recyclerview.R;
 
 import org.w3c.dom.Text;
 
@@ -15,7 +18,11 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_lista, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
@@ -36,6 +43,11 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            titulo = itemView.findViewById(R.id.textTitulo);
+            ano = itemView.findViewById(R.id.textAno);
+            genero = itemView.findViewById(R.id.textGenero);
+
         }
     }
 
